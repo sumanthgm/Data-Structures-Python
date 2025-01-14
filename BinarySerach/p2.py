d80c66237658bc63c56c844873221f12aeef383d
sumanth <sumanthgm756@gmail.com>
2025-01-14 16:53:02 +0530

    Adding Linear search

--- BinarySerach/p2.py
+++ BinarySerach/p2.py
@@ -1,1 +1,13 @@
-print("Program 2 Edite")
+# linear search
+def lsearch(arr,key):
+    for i in range (len(arr)):
+        if key == arr[i]:
+            return i
+    return -1
+
+
+arr=[10,20,30,40,50]
+key=int(input("Eneter the Key Input to search  :"))
+res=lsearch(arr,key)
+print(res)
+
